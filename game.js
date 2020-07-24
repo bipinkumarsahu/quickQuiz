80e7b1ee3b0b6be421344fe9f07e3dbee5338cca
Bipin Kumar Sahu <bipinsahu5@gmail.com>
2020-07-24 17:17:35 +0530

    Functionality

--- game.js
+++ game.js
@@ -55,6 +55,20 @@ getNewQuestion = () =>{
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice'+ number];
     })
+    availableQuesions.splice(questionIndex,1);
+    acceptingAnswers=true;
 }
+
+choices.forEach(choice =>{
+    choice.addEventListener('click',e =>{
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers=false;
+        const selectedChoice= e.target;
+        const selectedAnswer= selectedChoice.number;
+        getNewQuestion();
+    });
+   
+})
 
 startGame();
