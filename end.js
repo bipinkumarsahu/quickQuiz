2a632012edb30d08aed74a48716cc5d7aba4e1cf
Bipin Sahu <bipinsahu5@gmail.com>
2020-07-28 22:51:34 +0530

    highscore

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []; 
+//To either load the highscore from storage of available, or if using for the first time, initializes an empty array
+const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -9,5 +12,16 @@ username.addEventListener('keyup', () => {
 });
 
 saveHighScore = (e) => {
+    console.log('btn clicked!');
     e.preventDefault();
+    const score  ={
+        score: Math.floor(Math.random()*100),
+        user: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a,b) => b.score-a.score);
+    highScores.splice(5);
+
+    console.log(highScores);
 };
+
