5471679c00958558e257b6bc330e7c17ca929591
Bipin Sahu <bipinsahu5@gmail.com>
2020-07-28 23:58:06 +0530

    HighScore Setup

--- end.js
+++ end.js
@@ -15,13 +15,15 @@ saveHighScore = (e) => {
     console.log('btn clicked!');
     e.preventDefault();
     const score  ={
-        score: Math.floor(Math.random()*100),
+        score: mostRecentScore,
         user: username.value
     };
     highScores.push(score);
     highScores.sort((a,b) => b.score-a.score);
     highScores.splice(5);
 
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    //window.location.assign('/');
     console.log(highScores);
 };
 
